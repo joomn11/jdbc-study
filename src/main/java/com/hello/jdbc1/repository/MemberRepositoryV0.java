3d4323b75cf8602c1228ec5ccb530c3608523f6e
joomn11 <joomn11@gmail.com>
2022-11-18 11:15:33 +0900

    jdbc 개발 - 조회

--- src/main/java/com/hello/jdbc1/repository/MemberRepositoryV0.java
+++ src/main/java/com/hello/jdbc1/repository/MemberRepositoryV0.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -34,7 +35,36 @@ public class MemberRepositoryV0 {
         } finally {
             close(con, pstmt, null);
         }
+    }
 
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId= " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("DB Error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
     }
 
     private void close(Connection con, Statement stmt, ResultSet rs) {
--- src/test/java/com/hello/jdbc1/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/hello/jdbc1/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,25 @@
 package com.hello.jdbc1.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.hello.jdbc1.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
-    void save() throws SQLException {
+    void crud() throws SQLException {
+        // save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
