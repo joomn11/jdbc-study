89f733435a40785b9447c28a5cd3b4a43ef170a5
joomn11 <joomn11@gmail.com>
2022-11-21 11:06:38 +0900

    jdbc 개발 - 수정, 삭제

--- src/test/java/com/hello/jdbc1/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/hello/jdbc1/repository/MemberRepositoryV0Test.java
@@ -2,6 +2,7 @@ package com.hello.jdbc1.repository;
 
 import com.hello.jdbc1.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,21 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV012", 10000);
         repository.save(member);
 
-        //findById
+        // findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         Assertions.assertThat(findMember).isEqualTo(member);
+
+        // update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 }
