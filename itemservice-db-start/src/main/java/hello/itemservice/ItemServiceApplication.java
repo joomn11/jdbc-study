1484dd71273cd2ccf6a2a72acc2d56cda82b19ef
joomn11 <joomn11@gmail.com>
2023-01-13 12:40:30 +0900

    db - query dsl

--- itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.SpringDataJpaConfig;
+import hello.itemservice.config.QuerydslConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +15,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 @Slf4j
 public class ItemServiceApplication {
